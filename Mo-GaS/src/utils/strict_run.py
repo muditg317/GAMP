035581714cd1be3af8f95df0068d0a7a75fe0afc
Mudit Gupta <muditg317@gmail.com>
2022-12-07 02:43:43 -0500

    setup - fix strict run py to make sure executed from mogas root

--- Mo-GaS/src/utils/strict_run.py
+++ Mo-GaS/src/utils/strict_run.py
@@ -19,8 +19,13 @@ for frame in inspect.stack()[::-1]:
     break
 
 # enforce that the current working directory is the root of the project
-while os.getcwd().split(os.sep)[-1] != 'Mo-GaS':
-  os.chdir('..')
+if os.getcwd().split(os.sep)[-1] != 'Mo-GaS':
+  folder = os.getcwd()
+  while folder.split(os.sep)[-1] != 'Mo-GaS':
+    folder = os.path.dirname(folder)
+  relative_path = os.path.relpath(folder, os.getcwd())
+  print(f"Please run this script from the root of the Mo-GaS project ({relative_path})")
+  sys.exit(1)
 
 sys.path.append(os.getcwd())
 sys.path.append(os.path.dirname(os.path.abspath(main_file)))
